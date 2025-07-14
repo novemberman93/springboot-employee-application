c5f271f0a32d2d09528c5b3260e6e1b2d8b29f5a
Vishal <vishalraopvr@gmail.com>
2025-07-14 18:34:24 +0530

    code formatting

--- src/main/java/com/vishal/emp/employee/EmployeeApplication.java
+++ src/main/java/com/vishal/emp/employee/EmployeeApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class EmployeeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
     }
