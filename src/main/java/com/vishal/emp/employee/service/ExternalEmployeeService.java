3b9147c3717196fd7f0b5372d472917413b53599
Vishal <vishalraopvr@gmail.com>
2025-07-21 10:21:56 +0530

    comment 1 in nf2

--- src/main/java/com/vishal/emp/employee/service/ExternalEmployeeService.java
+++ src/main/java/com/vishal/emp/employee/service/ExternalEmployeeService.java
@@ -7,7 +7,7 @@ import java.util.Random;
 @Service
 public class ExternalEmployeeService {
     //just an external call to check working of circuit break pattern
-
+    //comment 1 in nf2
     public String callExternalService() {
         // Simulate failure
         if (new Random().nextBoolean()) {
